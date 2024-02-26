9cab1e738fd4997a79bda42079a96f8408fcf263
luka chakhvashvili <159915339+CHAXVASHVILILUKA@users.noreply.github.com>
2024-02-26 23:17:20 +0400

    Add files via upload

---
+++ day 1/homework/homework1.py
@@ -0,0 +1,48 @@
+from turtle import*
+width(5)  
+color("red")
+
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+#გავაკეთეთ კუბი
+
+#ვიწყებთ კარებს
+forward(70)
+left(90)
+forward(120)
+right(90)
+forward(50)
+right(90)
+forward(120)
+ #დავამთავრეთ კარები
+ 
+ #ვიწყებთ სახხურავს
+ 
+left(90)
+forward(81)
+left(90)
+forward(201)
+left(45)
+forward(120)
+left(80)
+forward(143)
+
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
