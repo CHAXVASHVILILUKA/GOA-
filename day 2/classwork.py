beb3db106b03f501d45293d28a1432d8111d90db
luka chakhvashvili <159915339+CHAXVASHVILILUKA@users.noreply.github.com>
2024-02-26 23:17:58 +0400

    Add files via upload

---
+++ day 2/classwork.py
@@ -0,0 +1,57 @@
+print("luka chakhvashvili")
+prnt("luka chakhvashvili")
+m ckvcs
+scscsc
+csvsvc 
+eee e exceptcee
+cccecccccecece 
+
+execcs\wwf
+else
+ exec
+ f s 
+ f s
+ EOFErrorf
+ setfee
+ finallyefes
+ FileExistsErrorsf
+ 
+ execsfes
+ finallysf
+ 
+ EOFErrore
+ finallyf
+ execf
+ setef
+ def
+ d  
+   
+   
+    
+      
+       
+       e 
+        
+        e
+        
+        exece
+        e
+         e
+          e
+          exece
+          exece
+          e 
+          e
+          exec
+          eef  execsc
+          s
+          classmethodcs
+          classmethodxcsd
+          classmethodc
+          dscs
+          
+          classmethodsdc
+          scxc
+          __spec__xc
+          elifs
+          sex
